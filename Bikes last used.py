6ac2686e29bc4f30e1c56838e9f06a7ad552bb6c
WAMBUI MUNGAI <112846283+Joanwambui@users.noreply.github.com>
2025-01-27 23:20:08 +0300

    Update Bikes last used.py

--- Bikes last used.py
+++ Bikes last used.py
@@ -5,6 +5,7 @@ result = dc_bikeshare_q1_2012.groupby('bike_number')['end_time'].max().to_frame(
 # Import your libraries
 import pandas as pd
 
+
 # Start writing code
 dc_bikeshare_q1_2012.head()
 
